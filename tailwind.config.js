930621c2322513628574279831712b2890a13f20
MrNithi <bossrocker777@gmail.com>
2021-08-15 21:21:54 +0700

    Add font inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /* eslint-disable */
+const plugin = require('tailwindcss/plugin');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   purge: [],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: colors.gray,
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
