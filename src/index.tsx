26af404e6792a2c4e2a70fff590e06994178bca5
MrNithi <bossrocker777@gmail.com>
2021-08-18 23:09:33 +0700

    Add Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Providers from 'Providers';
 import 'styles/index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>,
   document.getElementById('root'),
 );
