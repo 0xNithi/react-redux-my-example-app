c206046b03f218617c178116290c61a353565cc5
MrNithi <nithi.boss157@gmail.com>
2021-08-23 18:06:51 +0700

    Add classNames to Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import classNames from 'utils/classNames';
 
 interface Props {
   title: string;
+  className?: string;
 }
 
-const Layout: React.FC<Props> = ({ title, children }) => {
+const Layout: React.FC<Props> = ({ title, className, children }) => {
   return (
     <>
       <Helmet>
         <title>{title}</title>
       </Helmet>
-      <div className="p-2 mx-auto mt-20 w-full max-w-6xl">{children}</div>
+      <div className={classNames('p-2 mx-auto mt-20 w-full max-w-6xl', className)}>{children}</div>
     </>
   );
 };
