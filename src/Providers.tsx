f0aa078ec922b9ababeb5fb5c70097caf19e8b3b
MrNithi <nithi.boss157@gmail.com>
2021-08-26 23:04:41 +0700

    Remove componentMounted from ThemeProvider

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,27 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
 import store from 'state';
 import { useTheme } from 'state/user/hooks';
 
 const ThemeProvider: React.FC = ({ children }) => {
-  const [componentMounted, setComponentMounted] = useState<boolean>(false);
   const [isDark] = useTheme();
 
+  if (isDark) document.documentElement.classList.add('dark');
+  else document.documentElement.classList.remove('dark');
+
   useEffect(() => {
-    setComponentMounted(true);
     const timeOut = setTimeout(() => {
       document.documentElement.classList.add('nightwind');
     }, 100);
     return () => clearTimeout(timeOut);
   }, []);
 
-  useEffect(() => {
-    if (isDark) document.documentElement.classList.add('dark');
-    else document.documentElement.classList.remove('dark');
-  }, [isDark]);
-
-  if (!componentMounted) return <div />;
   return <>{children}</>;
 };
 
