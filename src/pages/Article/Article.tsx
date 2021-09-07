e0b611b66c2008fda38f2506c985459dd34f6c71
Nithi Nomprawat <nithi.boss157@gmail.com>
2021-09-07 23:01:52 +0700

    Add Switch for Article Route

--- src/pages/Article/Article.tsx
+++ src/pages/Article/Article.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import New from './New';
-import Edit from './Edit';
+import { Switch, Route } from 'react-router-dom';
+
+const New = React.lazy(() => import('./New'));
+const Edit = React.lazy(() => import('./Edit'));
+const NotFound = React.lazy(() => import('pages/NotFound'));
 
 const Article: React.FC = () => {
   return (
-    <>
+    <Switch>
       <Route exact path="/article/new" component={New} />
       <Route path="/article/edit/:articleId" component={Edit} />
-    </>
+      <Route component={NotFound} />
+    </Switch>
   );
 };
 
