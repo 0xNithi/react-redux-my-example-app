6bfe6d7539cba587981a5c71195147b37ec72e3a
Nithi Nomprawat <nithi.boss157@gmail.com>
2021-09-10 14:30:29 +0700

    Add Link to ArticleCard

--- src/pages/Home/components/ArticleCard.tsx
+++ src/pages/Home/components/ArticleCard.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Article } from 'state/types';
 
 interface Props {
   article: Article;
 }
 
-const ArticleCard: React.FC<Props> = ({ article }) => {
-  return (
-    <div className="p-4 bg-white rounded border border-gray-200 dark:bg-gray-800">
-      <div className="text-2xl font-semibold text-black">{article.title}</div>
-      <div className="text-base font-normal text-gray-500">{article.body}</div>
-    </div>
-  );
-};
+const ArticleCard: React.FC<Props> = ({ article }) => (
+  <Link to={`/article/${article.id}`} className="block p-4 bg-white rounded border border-gray-200 dark:bg-gray-800">
+    <div className="text-2xl font-semibold text-black">{article.title}</div>
+    <div className="text-base font-normal text-gray-500">{article.body}</div>
+  </Link>
+);
 
 export default ArticleCard;
