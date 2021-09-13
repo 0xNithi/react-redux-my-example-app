03524096aa1448791b8a66e2704d5310642a5aef
Nithi Nomprawat <nithi.boss157@gmail.com>
2021-09-13 19:36:21 +0700

    Format files

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,6 +6,7 @@ import SkeletonCard from './components/SkeletonCard';
 
 const Home: React.FC = () => {
   const { articles, isLoading } = useFetchArticles();
+
   return (
     <Layout title="Home | My Example App" className="space-y-4">
       {!isLoading && articles.map((article) => <ArticleCard article={article} key={article.id} />)}
--- src/pages/Home/components/SkeletonCard.tsx
+++ src/pages/Home/components/SkeletonCard.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import Skeleton from 'components/Skeleton';
 
-const SkeletonCard: React.FC = () => {
-  return (
-    <div className="block p-4 space-y-2 bg-white rounded border border-gray-200 dark:bg-gray-800">
-      <Skeleton background="bg-black" width="w-48" height="h-6" />
-      <Skeleton width="w-80" height="h-4" />
-      <Skeleton width="w-64" height="h-4" />
-      <Skeleton width="w-72" height="h-4" />
-      <Skeleton width="w-32" height="h-4" />
-    </div>
-  );
-};
+const SkeletonCard: React.FC = () => (
+  <div className="block p-4 space-y-2 bg-white rounded border border-gray-200 dark:bg-gray-800">
+    <Skeleton background="bg-black" width="w-48" height="h-6" />
+    <Skeleton width="w-80" height="h-4" />
+    <Skeleton width="w-64" height="h-4" />
+    <Skeleton width="w-72" height="h-4" />
+    <Skeleton width="w-32" height="h-4" />
+  </div>
+);
 
 export default SkeletonCard;
